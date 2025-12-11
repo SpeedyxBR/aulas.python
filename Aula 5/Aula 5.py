0363f0a05cc0f2820eedc1c06d5fa970c574c5e4
SpeedyxBR <andrel.cilva57@gmail.com>
2025-12-11 09:17:31 -0300

    feat: Adiciona Aula 5 - Exercício de variáveis e tipos de dados

---
+++ Aula 5/Aula 5.py
@@ -0,0 +1,24 @@
+'''
++
+-
+*
+/
+//
+**
+%
+()
+( n + n ) - Os parênteses têm a maior precedência, contas dentro deles são realizadas primeiro
+
+** - Depois vem a exponenciação
+
+* / //  % - Na sequência multiplicação, divisão, divisão inteira e módulo
+
++  - - Por fim, soma e subtração
+'''
+print(10 * 10) #Multiplicação
+print(10 + 10) #Adição
+print(10 - 5)  #Subtração
+print(10 / 2)  #Divisão
+
+print(10 * 'Andrel')
+2'1'
