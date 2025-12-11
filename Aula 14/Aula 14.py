1de7c7d179d6e9c30f78e96162e692baa2720293
SpeedyxBR <andrel.cilva57@gmail.com>
2025-12-11 09:18:21 -0300

    feat: Adiciona Aula 14 - Introdução às estruturas condicionais if, elif, else

---
+++ Aula 14/Aula 14.py
@@ -0,0 +1,13 @@
+"""
+While em Python - Aula 7
+utilizado para realizar ações enquanto
+uma condição for verdadeira.
+
+Requisitos: Entender Condições e Operadores
+"""
+x = 0
+while x < 100:
+    print(x)
+    x = x + 1
+
+print('Acabou!')
---
+++ Aula 14/Cont 2.py
@@ -0,0 +1,14 @@
+#       índices
+#       0123456789....................33
+
+frase = 'o rato roeu a roupa do rei de roma'
+tamanho_frase = len(frase)
+contador = 0
+nova_string = ''
+
+while contador < tamanho_frase:
+    print(frase[contador], contador)
+    nova_string += frase[contador]
+    contador += 1
+
+print(nova_string)
