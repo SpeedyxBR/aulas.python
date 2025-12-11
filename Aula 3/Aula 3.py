a25f741ee5f891c12281e48ecaf4a1ff05ae56da
SpeedyxBR <andrel.cilva57@gmail.com>
2025-12-11 09:17:00 -0300

    feat: Adiciona Aula 3 - Variáveis e operadores aritméticos

---
+++ Aula 3/Aula 3.py
@@ -0,0 +1,21 @@
+# Aula 3: Variáveis e Operadores Aritméticos
+
+# Declaração de variáveis para armazenar números
+numero1 = 10
+numero2 = 3
+
+# Realizando operações aritméticas com as variáveis
+soma = numero1 + numero2
+subtracao = numero1 - numero2
+multiplicacao = numero1 * numero2
+divisao = numero1 / numero2
+resto = numero1 % numero2
+potencia = numero1 ** numero2
+
+# Imprimindo os resultados com uma descrição
+print("O resultado da Soma de 10 + 3 é:", soma)
+print("O resultado da Subtração de 10 - 3 é:", subtracao)
+print("O resultado da Multiplicação de 10 * 3 é:", multiplicacao)
+print("O resultado da Divisão de 10 / 3 é:", divisao)
+print("O Resto da divisão de 10 por 3 é:", resto)
+print("O resultado da Potência de 10 elevado a 3 é:", potencia)
