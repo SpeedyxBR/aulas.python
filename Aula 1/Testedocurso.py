76e35a88e03190d73a4afadf0afb7369fd80566a
SpeedyxBR <andrel.cilva57@gmail.com>
2025-12-11 09:16:27 -0300

    feat: Adiciona Aula 1 - Introdução ao print e comentários

---
+++ Aula 1/Testedocurso.py
@@ -0,0 +1,9 @@
+'''print("ola mundo 1")
+print("ola mundo 2")
+#Falando do que se trata nessa linha
+print("ola mundo 3")'''
+
+print("ola mundo 4")
+print("ola mundo 4")
+print("ola mundo 4")
+# Isso é um comentário
