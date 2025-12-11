e94aa10ce8d9c326ff43ca73da475566450057a6
SpeedyxBR <andrel.cilva57@gmail.com>
2025-12-11 09:18:22 -0300

    feat: Adiciona Aula 15 - Operadores relacionais (lógicos)

---
+++ Aula 15/Aula 15.py
@@ -0,0 +1,14 @@
+"""
+For in em Python
+Iterando Strings com For
+Função range(start=0, stop, step=1)
+"""
+
+for n in range(20,10, -1):
+    print(n)
+else:
+    print('Acabou')
+
+for n in range(100):
+    if n % 8 == 0:
+        print(n)
