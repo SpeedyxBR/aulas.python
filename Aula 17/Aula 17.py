cbb8b4bdee673f3f3f88a59136c8d3e70afd33d4
SpeedyxBR <andrel.cilva57@gmail.com>
2025-12-11 09:18:22 -0300

    feat: Adiciona Aula 17 - Exercício de sistema de login simples

---
+++ Aula 17/Aula 17.py
@@ -0,0 +1,22 @@
+"""
+Split, Join, Enumerate em Python
+* Split - Dividir uma String # str
+* Join - Juntar uma Lista # str
+* Enumerate - Enumerar elementos da lista # list / iteráveis
+"""
+
+lista = [
+        ['Edu', 'Luiz', 'Andrel '],
+        ['Edu', 'Luiz', 'Andrel '],
+        ['Edu', 'Luiz', 'Andrel '],
+        ['Edu', 'Luiz', 'Andrel '],
+        ['Edu', 'Luiz', 'Andrel '],
+        ['Edu', 'Luiz', 'Andrel '],
+        ['Edu', 'Luiz', 'Andrel ']
+
+]
+
+
+
+for v1, v2 in enumerate(lista):
+    print(v1, v2)
