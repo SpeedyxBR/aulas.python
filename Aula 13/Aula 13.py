292c73259b0d262c6b189d77dbdeb3b3cabc8908
SpeedyxBR <andrel.cilva57@gmail.com>
2025-12-11 09:18:21 -0300

    feat: Adiciona Aula 13 - Exercício de calculadora simples com input()

---
+++ Aula 13/Aula 13.py
@@ -0,0 +1,17 @@
+"""
+Manipulando strings - Aula 6
+* String Indices
+* Fatiamento de Strings [Inicio:Fim:Passo]
+* Funções Built-in len, abs, type, print etc...
+Essas funções podem ser usadas diariamente em cada tipo.
+
+Você pode conferir tudo isso em:
+https://docs.python.org/3/library/functions.html
+https://docs.python.org/3/library/stdtypes.html
+"""
+# Positivos [012345678]
+texto = 'Python s2'
+# Negativos -[9876543210]
+
+url = 'www.google.com.br/'
+print (texto[:-1])
