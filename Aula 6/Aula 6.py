9f6e0ab5aa09a64faf6b59f871a82ab8bb91f362
SpeedyxBR <andrel.cilva57@gmail.com>
2025-12-11 09:18:18 -0300

    feat: Adiciona Aula 6 - Concatenação e repetição de strings

---
+++ Aula 6/Aula 6.py
@@ -0,0 +1,21 @@
+'''
+iniciar com letra, pode conter números, separar_, letras minúsculas
+'''
+
+nome = 'Andrel'
+idade = 20  #int
+altura = 1.90  # float
+e_maior = idade > 18  # bool
+data_1 = True
+data_atual = 2022
+peso = 115
+imc = peso / (altura ** 2)
+
+print('Nome:', nome)
+print('Idade:', idade)
+print('Altura:', altura)
+print('É maior:', e_maior)
+
+print(idade * altura)
+print(nome, 'Tem', idade,'Anos de Idade', altura, e_maior, peso, imc)
+
