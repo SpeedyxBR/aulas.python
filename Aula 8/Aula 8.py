cf52c4addeebce67cf6e155a6838cbc865129b28
SpeedyxBR <andrel.cilva57@gmail.com>
2025-12-11 09:18:20 -0300

    feat: Adiciona Aula 8 - Exercício de cálculo do IMC

---
+++ Aula 8/Aula 8.py
@@ -0,0 +1,15 @@
+"""
+Entrada de Dados - Aula 3
+"""
+nome = input('Qual o seu Nome? ')
+idade = input('Qual a sua idade? ')
+ano_nascimento = 2022-int(idade)
+
+print()
+print(f'{nome} tem {idade} anos. ' 
+      f'{nome} nasceu em {ano_nascimento}.')
+
+numero_1 = input('Digite um número: ')
+numero_2 = input('Digite outro número: ')
+
+print(numero_1 + numero_2)
